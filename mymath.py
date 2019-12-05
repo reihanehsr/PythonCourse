d735a97a103400706b864527f7f630bd0d439375
reihanehsr <56694184+reihanehsr@users.noreply.github.com>
2019-12-05 18:10:20 -0500

    Library
    Library

---
+++ mymath.py
@@ -0,0 +1,15 @@
+def add(a,b):
+    return a+b
+
+
+def sub(c,d):
+    print(c)
+    print(d)
+    # Perform c - d
+    return(c-d)
+
+def multi(a,b):
+    return a*b
+
+def div(x,y):
+    return x/y
