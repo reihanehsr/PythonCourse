fcfde9c23c503f8648fe26c7a642e93eab1b3e3b
reihanehsr <56694184+reihanehsr@users.noreply.github.com>
2019-12-05 19:23:22 -0500

    Dec5

--- Dec5.py
+++ Dec5.py
@@ -3,3 +3,31 @@ from mymath import *
 print (add(2,4))
 
 print (div(10,5))
+
+# training on if/or
+a = 5
+# equal is ==
+if a == 3 and a < 5:
+    print ("this is impossible")
+
+if a == 3:
+    print ("a is 3")
+elif a == 4:
+    print ("a is 4")
+# When there is third expression involved, use else
+else:
+    print ("Something Else")
+
+# when you write if, elif, always include esle
+
+def decision (age1, age2):
+    result = (age1/2)+7
+    if age2 < result:
+        return ("too young")
+    else:
+        return ("Perfect")
+
+# print (decision(45,18))
+
+assert decision(45,18) == "too young" and decision(14,14) == "Perfect"
+assert decision(95, 35) == "Perfect"
